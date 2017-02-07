2af1d3162618357e2cf93bd2ff92f4b54d3bfd2f
kyhou <joelskill@gmail.com>
2017-02-07 19:12:23 -0200

    First Commit
    Created utilizing tutorial from the channel gamesplusjames, until
    chapter 40.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Slider HpBar, ExpBar;
+    public Text HpText, ExpText, lvlText;
+    public PlayerHealthManager playerHP;
+    private PlayerStats playerStats;
+
+    private static bool UIExists;
+
+    void Start()
+    {
+        if (!UIExists)
+        {
+            UIExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        playerStats = GetComponent<PlayerStats>();
+    }
+
+    void Update()
+    {
+        HpBar.maxValue = playerHP.maxHP;
+        HpBar.value = playerHP.currentHP;
+        HpText.text = "HP: " + playerHP.currentHP + "/" + playerHP.maxHP;
+
+        if (playerStats.currentLvl < playerStats.expToLvlUp.Length)
+        {
+
+            ExpBar.maxValue = playerStats.expToLvlUp[playerStats.currentLvl];
+            ExpText.text = "Exp: " + playerStats.currentExp + "/" + playerStats.expToLvlUp[playerStats.currentLvl];
+            lvlText.text = "LvL: " + playerStats.currentLvl;
+        }
+
+        ExpBar.value = playerStats.currentExp;
+        
+    }
+}
